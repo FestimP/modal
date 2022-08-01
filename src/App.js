0f6e13919921264331533564248a8d198c59a9e5
FestimsRepo <102750865+FestimP@users.noreply.github.com>
2022-08-01 16:36:38 +0200

    aa

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Page from "./components/page/page";
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <Page/>
+    </div>
+  );
+}
+
+export default App;
